c1371c160173cb7ad1f767bfd42447d4925d325a
Rahman2782 <rahman2782@gmail.com>
2025-05-16 03:16:37 -0400

    Update lab1.py

--- LAB01/lab1.py
+++ LAB01/lab1.py
@@ -22,12 +22,12 @@ def wins_rock_scissors_paper(s1, s2):
 
 
 # --- 2 - factorial function --- #
-def factorial(i):
+def factorial(n):
     result = 1
-    if i == 0:
+    if n == 0:
         return 1
     else:
-        for x in range(1, i+1):
+        for x in range(1, n+1):
             result *= x 
         return result 
 
@@ -36,3 +36,28 @@ def factorial(i):
 
 
 
+# --- 3 - Fibonacci --- #
+def fiboncacci(n):
+    return True
+
+
+
+
+
+
+
+
+
+
+# --- --- REFLECTION --- ---
+#While python functions in a similar way to C++, it is a higher level language and
+#as a result makes the code much more readable and smaller in comparison to C++. For
+#example, declaring variables in C++ requires you to specify the type of the variable. In
+#python I dont have to do that. The for loop is another example of a tool that is much more
+#simple in python in comparison to C++. 
+
+#The entire structure and flow of python behaves differently from what I expected, for example 
+#there are no semi-colons at all which was very off putting at first, each line is its own 
+#statement automatically. Furthermore not having brackets when declaring an if-statement or 
+#for-loop was very strange at first. These read more like a sentence then code now, if that
+#makes sense.
